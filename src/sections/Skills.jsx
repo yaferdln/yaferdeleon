795c7ec93f8f1c2cb7fcd320112c714190d8087e
yaferdln <deleonyafer@gmail.com>
2025-05-18 06:24:23 +0800

    initial commit

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -4,7 +4,7 @@ const Skills = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
       {SKILLS_DATA.map((skill, index) => (
-        <div key={index} className="bg-slate-800/40 rounded px-8 py-4">
+        <div key={index}>
           <h3 className="text-md md:text-lg font-semibold mb-6 text-center tracking-wide text-slate-300">
             {skill.category}
           </h3>
@@ -13,7 +13,7 @@ const Skills = () => {
             {skill.items.map((item, idx) => (
               <span
                 key={idx}
-                className="inline-flex items-center bg-slate-800/40 rounded-full px-4 py-2 mb-3 text-sm md:text-md font-medium text-slate-300 transition-colors duration-300 hover:bg-teal-400 hover:text-slate-900 cursor-default select-none"
+                className="inline-flex items-center bg-slate-800/40 rounded-full px-4 py-2 mb-3 text-xs md:text-xs  text-slate-300 transition-colors duration-300 hover:bg-teal-400 hover:text-slate-900 cursor-default select-none"
               >
                 {item.icon && <item.icon className="mr-2 text-lg" />}
                 {item.name}
